fadfcd820498aa3e7c01be3169221ae7ce05ccb3
akase244 <akase244@gmail.com>
2017-01-24 18:21:48 +0900

    fix don\'t work play music.

--- getwild.js
+++ getwild.js
@@ -1,8 +1,8 @@
-setTimeout(function(){
+setInterval(function(){
     var date   = new Date();
     var hour   = parseInt(date.getHours(), 10);
     var minute = parseInt(date.getMinutes(), 10);
     if (hour == 19 && minute == 0) {
         new Audio('getwild.mp3').play();
     }
-}, 30 * 1000);
+}, 1000 * 60);
